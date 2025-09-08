b8d60d8cc4c61c3b9c514c520797b0bddcc20180
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-09-08 19:52:39 +0200

    define experimental option

---
+++ crates/nu-experimental/src/options/database_cmd_next.rs
@@ -0,0 +1,29 @@
+use crate::*;
+
+/// Use new implementation of the database-related commands.
+///
+/// This replaces the old implementation of:
+/// - `into sqlite`
+/// - `query db`
+/// - `query`
+/// - `schema`
+///
+/// The new implementation is built completely separately next to the old one, so any changes
+/// shouldn't affect the old command.
+///
+/// Enabling this command might replace commands with other names and therefore possibly breaking
+/// scripts.
+pub static DATABASE_CMD_NEXT: ExperimentalOption = ExperimentalOption::new(&DatabaseCmdNext);
+
+// No documentation needed here since this type isn't public.
+// The static above provides all necessary details.
+struct DatabaseCmdNext;
+
+impl ExperimentalOptionMarker for DatabaseCmdNext {
+    const IDENTIFIER: &'static str = "database-cmd-next";
+    const DESCRIPTION: &'static str = concat!(
+        "Use a new implementation of the database-related commands. ",
+        "May contain breaking changes."
+    );
+    const STATUS: Status = Status::OptIn;
+}
