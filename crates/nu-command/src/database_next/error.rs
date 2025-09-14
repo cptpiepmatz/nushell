421c58104d079cfc4454f9b2ac616be6cc5f774e
Tim 'Piepmatz' Hesse <git+github@cptpiepmatz.de>
2025-09-14 13:01:26 +0200

    add more error variants

--- crates/nu-command/src/database_next/error.rs
+++ crates/nu-command/src/database_next/error.rs
@@ -1,8 +1,10 @@
-use std::string::FromUtf8Error;
+use std::{borrow::Cow, string::FromUtf8Error};
 
-use nu_protocol::{shell_error::io::IoError, Span, Type, Value};
+use nu_protocol::{Span, Type, Value, shell_error::io::IoError};
 
-use crate::database_next::plumbing::{sql::SqlString, storage::DatabaseStorage};
+use crate::database_next::plumbing::{
+    decl_type::DatabaseDeclType, sql::SqlString, storage::DatabaseStorage,
+};
 
 pub enum DatabaseError {
     OpenConnection {
@@ -40,10 +42,22 @@ pub enum DatabaseError {
         span: Span,
     },
 
+    // mark this variant as deprecated to find missing pieces
+    Todo {
+        msg: Cow<'static, str>,
+        span: Span,
+    },
+
     Io(IoError),
 
     FromUtf8 {
         span: Span,
         error: FromUtf8Error,
+    },
+
+    InvalidDeclType {
+        value: rusqlite::types::Value,
+        decl_type: DatabaseDeclType,
+        span: Span,
     },
 }
