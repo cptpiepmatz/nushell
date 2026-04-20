5d0ae2c31ab12160722fd949b7a997e53d68ca1c
Tim 'Piepmatz'  Hesse <git+github@cptpiepmatz.de>
2026-04-20 17:43:40 +0200

    get block id of custom command

--- crates/nu-test/src/discover.rs
+++ crates/nu-test/src/discover.rs
@@ -1,3 +1,5 @@
+use std::any::Any;
+
 use nu_engine::scope::ScopeData;
 use nu_protocol::{
     Id, Span,
@@ -9,13 +11,11 @@ fn discover() {
     let mut engine_state = super::engine_state();
     let stack = Stack::new();
     let mut working_set = StateWorkingSet::new(&engine_state);
-    let code = b"overlay new testing; use tests/example.nu";
+    let code = b"overlay new testing; source tests/example.nu";
     let block = nu_parser::parse(&mut working_set, None, code, false);
     assert!(working_set.parse_errors.is_empty());
     assert!(working_set.compile_errors.is_empty());
     engine_state.merge_delta(working_set.delta).unwrap();
-
-    dbg!(engine_state.scope.overlays.iter().map(|(name, _)| str::from_utf8(name).unwrap()).collect::<Vec<_>>());
 
     let command_attributes: Vec<_> = engine_state
         .scope
@@ -23,7 +23,10 @@ fn discover() {
         .map(|overlay| overlay.decls.values())
         .flatten()
         .map(|decl_id| engine_state.get_decl(*decl_id))
-        .map(|command| (command.name(), command.attributes(), command.decl_span()))
+        .inspect(|command| {dbg!(command.name());})
+        .flat_map(|command| command.block_id().map(|block_id| (command, block_id)))
+        .map(|(command, block_id)| (command.name(), command.attributes(), engine_state.get_block(block_id).span))
+        .map(|(name, attributes, span)| engine_state.try_get_file_contents(span.unwrap()).map(str::from_utf8))
         .collect();
 
     dbg!(command_attributes);
